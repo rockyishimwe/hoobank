99dd50b9d44fb4d4c1af74db22ec9c1038b18b24
rocky ishimwe <rockyishimwe9@gmail.com>
2026-05-21 07:55:27 +0200

    First commit

---
+++ src/constants/index.js
@@ -0,0 +1,31 @@
+import {people01,people02,people03,facebook,instagram,linkedin,twitter,airbnb,binance,coinbase,dropbox,send,shield,star} from '../assets'
+export const navLinks = [
+    {
+        id:'home',
+        title:'Home',
+    },
+    {
+        id:'features',
+        title:'Features',
+    },
+    {
+        id:'product',
+        title:'Product',
+    },
+    {
+        id:'clients',
+        title:'Clients',
+    },
+];
+export const features = [
+    {
+        id:'feature-1',
+        icon:star,
+        title:'Rewards',
+        content:
+        "The best credit cards offer some tantalizing combinations of promotions and prizes",
+    },
+    {
+        id:"feature-2"
+    }
+]
