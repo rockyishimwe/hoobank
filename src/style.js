83e365c6b3161a5a6f40625e1de31dff10e0ed66
rocky ishimwe <rockyishimwe9@gmail.com>
2026-05-22 21:31:08 +0200

    implemented the whole styles need

--- src/style.js
+++ src/style.js
@@ -7,6 +7,16 @@ const styles = {
 
     paddingX:"sm:px-16 px-6",
     paddingY:"sm:py-16 py-6",
-    padding:"sm:px-16 sm:py-12 py-4"
-}
+    padding:"sm:px-16 sm:py-12 py-4",
+
+    marginX:"sm:mx-16 mx-6",
+    marginY:"sm:my-16 my-6",
+};
+export const layout = {
+    section:`flex md:flex-row flex-col ${styles.paddingY}`,
+    sectionImgReverse:`flex-1 flex ${styles.flexCenter} md:mr-10 mr-0 md:mt-0 mt-10 relative`,
+    sectionImg:`flex-1 flex ${styles.flexCenter}
+    md:ml-10 ml-0 md:mt-0 mt-10 relative`,
+    sectionInfo:`flex-1 ${styles.flexStart} flex-col`,
+};
 export default styles;
