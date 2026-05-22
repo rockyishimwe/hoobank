90e2165ba53bcd9afd91f644355e210f5b60c059
rocky ishimwe <rockyishimwe9@gmail.com>
2026-05-22 19:53:05 +0200

    starting with app.js file

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import styles from "./style";
+import { Billing,Business,CardDeal,Clients,CTA,Footer,Hero,Navbar,Stats,Testimonials } from "./components";
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar/>
+        </div>        
+      </div>
+    </div>
   )
 }
 
-export default App
+export default App;
