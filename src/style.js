de4c03b013c378371add7e19949eaae6f303c4ed
rocky ishimwe <rockyishimwe9@gmail.com>
2026-05-22 20:58:58 +0200

    fixed a bug

---
+++ src/style.js
@@ -0,0 +1,5 @@
+const styles = {
+    boxWidth:"xl:max-[1280px] w-full",
+    heading2:"font-poppins font-semibold xs:text-[48px] text-[40px] text-white xs:leading-[76.8px] leading-[66.8px] w-full",
+}
+export default styles;
