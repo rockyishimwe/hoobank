b91c22bf9a2da6f3bf1c181e3148de30a55491ad
rocky ishimwe <rockyishimwe9@gmail.com>
2026-05-23 19:34:27 +0200

    fixed a rendering error bug

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,7 @@ import Navbar from "./Navbar";
 import Billing from "./Billing";
 import CardDeal from "./CardDeal";
 import Business from "./Business";
+import Clients from "./Clients";
 import CTA from "./CTA";
 import Stats from "./Stats";
 import Footer from "./Footer";
@@ -12,6 +13,7 @@ export {
     Billing,
     CardDeal,
     Business,
+    Clients,
     CTA,
     Stats,
     Footer,
