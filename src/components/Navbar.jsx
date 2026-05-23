4de17ee5d99eb11807a69b0cc5457adc624f31e5
rocky ishimwe <rockyishimwe9@gmail.com>
2026-05-23 08:16:54 +0200

    added some hooks to use in the navbar section

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+import {useState} from "react";
+import {close,logo,menu} from "../assets";
+import {navLinks} from "../constants";
+const Navbar = () => {
+    const [active,setActive] = useState("Home");
+    const [toggle,setToggle] = useState(false);
+}
