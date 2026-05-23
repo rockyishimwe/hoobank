f17dd2a43dfb162798c6dd2ef926ac7ab31096aa
rocky ishimwe <rockyishimwe9@gmail.com>
2026-05-23 19:14:00 +0200

    Fixed a bug

---
+++ src/components/Hero.jsx
@@ -0,0 +1,10 @@
+import styles from "../style";
+import {discount,robot} from "../assets";
+import GetStarted from "./GetStarted";
+const Hero = () =>{
+    return(
+        <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
+
+        </section>
+    )
+}
