60ee4d6f8db0989e4cb9aeaac30756c6775c8b1e
rocky ishimwe <rockyishimwe9@gmail.com>
2026-05-23 08:19:34 +0200

    added image to the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,4 +4,9 @@ import {navLinks} from "../constants";
 const Navbar = () => {
     const [active,setActive] = useState("Home");
     const [toggle,setToggle] = useState(false);
+    return(
+        <nav className="w-full flex py-6 justify-between items-center navbar">
+            <img src={logo} alt="hoobank" className="w-[124px] h-[32px]"/>
+        </nav>
+    )
 }
