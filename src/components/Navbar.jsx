b8b112e47078b7f2c1eb7a6134629de070b0cf60
rocky ishimwe <rockyishimwe9@gmail.com>
2026-05-23 08:28:16 +0200

    Implemented the first navbar item

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,15 @@ const Navbar = () => {
     return(
         <nav className="w-full flex py-6 justify-between items-center navbar">
             <img src={logo} alt="hoobank" className="w-[124px] h-[32px]"/>
+            <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+                {navLinks.map((nav,index)=>(
+                    <li className={`font-poppins font-normal cursor-pointer  text-[16px] ${active === nav.title ? "text-white" : "text-dimWhite"} ${index === navLinks.length - 1 ? "mr-0" : "mr-10"}`}
+                    key={nav.id}
+                    onClick={()=> setActive(nav.title)}>
+                        <a href={`#${nav.id}`}>{nav.title}</a>                        
+                    </li>
+                ))}
+            </ul>
         </nav>
     )
 }
