75a20d2fdaf1a0f337ca70073786a27861fe2d0f
rocky ishimwe <rockyishimwe9@gmail.com>
2026-05-23 22:03:05 +0200

    added some basics of stats card

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -2,8 +2,12 @@ import {stats} from "../constants";
 import styles from "../style";
 
 const Stats = () => (
-  <section> 
-
+  <section className={`${styles.flexCenter} flex-row flx-wrap sm:mb-20 mb-6`}> 
+  {stats.map((stat)=>(
+    <div key={stat.id} className={`flex-1 flx justify-start items-center flex-row m-3`}>
+      <h4 className="font-poppins font-semibold xs:text-[40.89px] text-[30.89px] xs:leading-[53.16px] leading-[43.16px] text-white">{stat.value}</h4>
+    </div>
+  ))}
   </section>
 );
 
