438893587bdfdd212ff7f59edcd6bb0997613d0c
rocky ishimwe <rockyishimwe9@gmail.com>
2026-05-23 20:33:11 +0200

    added a description of text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,10 +17,19 @@ const Hero = () =>{
                         The Next <br className="sm:block hidden"/>{" "}
                         <span className="text-gradient">Generation</span>{" "}
                     </h1>
-                    <div className="ss:flex hiddern md:mr-4 mr-0">
+                    <div className="ss:flex hidden md:mr-4 mr-0">
                         <GetStarted/>
                     </div>
                 </div>
+                <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] w-full">
+                    Payment Method.
+                </h1>
+                <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+                    Our team of experts uses a methodology to identify the credit cards most likely to fit your needs.We examine annual percentage rates, annual fees.
+                </p>
+            </div>
+            <div>
+                
             </div>
         </section>
     )
