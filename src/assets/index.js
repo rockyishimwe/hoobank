423750fbe5e9366b34940dd2d485de689a2aba97
rocky ishimwe <rockyishimwe9@gmail.com>
2026-05-23 08:34:45 +0200

    fixed a bug

--- src/assets/index.js
+++ src/assets/index.js
@@ -20,7 +20,7 @@ import facebook from "./facebook.svg";
 import instagram from "./instagram.svg";
 import linkedin from "./linkedin.svg";
 import twitter from "./twitter.svg";
-import people01 from "./people01.png";
+import people01 from "./people01.jpg";
 import people02 from "./people02.png";
 import people03 from "./people03.png";
 
