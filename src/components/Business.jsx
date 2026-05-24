c21b350c466b4cdf8b01c023a0a2593e288f5eb9
rocky ishimwe <rockyishimwe9@gmail.com>
2026-05-24 10:06:29 +0200

    started styling

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -3,13 +3,15 @@ import {features} from "../constants";
 import styles,{layout} from "../style";
 import Button from "./Button";
 const FeatureCard = ({icon,title,content ,index})=>(
-  <section>
-
-  </section>
+  <div className={`flex flex-row p-6 rounded-[20px] ${index ! == features.length-1 ? "mb-6":"mb-0"} feature-card`}>
+    <div>
+      
+    </div>
+  </div>
 );
 const Business = () => (
   <section>
-    
+
   </section>
 );
 export default Business;
