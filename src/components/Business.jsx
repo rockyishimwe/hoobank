6df2ed53c068d719722548c6dc43406090aaabdd
rocky ishimwe <rockyishimwe9@gmail.com>
2026-05-24 09:45:54 +0200

    starting the business section

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -1,5 +1,15 @@
-const Business = () => {
-  return <div>Business</div>;
-};
+import { Business } from ".";
+import {features} from "../constants";
+import styles,{layout} from "../style";
+import Button from "./Button";
+const FeatureCard = ({icon,title,content ,index})=>(
+  <section>
 
+  </section>
+);
+const Business = () => (
+  <section>
+    
+  </section>
+);
 export default Business;
