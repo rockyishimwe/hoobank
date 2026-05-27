5c03b6f1fb9645511ebeafec7cea65408ca68aa6
rocky ishimwe <rockyishimwe9@gmail.com>
2026-05-27 07:48:18 +0200

    added some description

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -4,9 +4,15 @@ import Button from "./Button";
 const CardDeal = () => (
   <section className={layout.section}>
     <div className={layout.sectionInfo}>
-      <h2>
-        
+      <h2 className={styles.heading2}>
+        Find a better card deal <br className="sm:block hidden" /> in few easy
+        steps.       
       </h2>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Discover smarter ways to manage your finances with exclusive card
+  benefits, lower transaction fees, and secure digital payments
+  all designed to give you more control over your money.
+      </p>
     </div>
   </section>
 );
