3f5fae35ebb76a5adb6460ae7317b234e4251821
rocky ishimwe <rockyishimwe9@gmail.com>
2026-05-27 09:50:27 +0200

    started styling some layers of testimonial section

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -2,7 +2,11 @@ import {feedbak} from "../constants";
 import styles from "../style";
 import FeedbackCard from "./FeedbackCard";
 const Testimonials = () => {
-  return <div>Testimonials</div>;
+  <section id="clients" className={`${styles.paddingY} ${styles.flexCenter} flex-col relative `}>
+    <div className="absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient bottom-40">
+
+    </div>
+  </section>
 };
 
 export default Testimonials;
