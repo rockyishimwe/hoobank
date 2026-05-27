71bcc80b94ebd17643defe532e4925bd4a7a6d33
rocky ishimwe <rockyishimwe9@gmail.com>
2026-05-27 09:41:18 +0200

    starting the implementation of testimonials page

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,3 +1,6 @@
+import {feedbak} from "../constants";
+import styles from "../style";
+import FeedbackCard from "./FeedbackCard";
 const Testimonials = () => {
   return <div>Testimonials</div>;
 };
