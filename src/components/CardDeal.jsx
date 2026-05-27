2c588e84f2dcb0705ac6e8f220f04c106116b704
rocky ishimwe <rockyishimwe9@gmail.com>
2026-05-27 07:45:40 +0200

    :fixed a bug

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,8 +1,14 @@
 import {card} from "../assets";
 import styles, {layout} from "../style";
 import Button from "./Button";
-const CardDeal = () => {
-  return <div>CardDeal</div>;
-};
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2>
+        
+      </h2>
+    </div>
+  </section>
+);
 
 export default CardDeal;
