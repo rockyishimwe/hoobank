6412e391ade130052860fcb74ba46b4bd87fdf6a
rocky ishimwe <rockyishimwe9@gmail.com>
2026-05-28 16:47:40 +0200

    :fixed a bug

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import styles from "../style";
 import {logo} from "../assets";
-import {footerLinks, socialMedia} from "../constants";
+import {footerLinks} from "../constants";
 const Footer = () => (
   <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
     <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
@@ -10,15 +10,25 @@ const Footer = () => (
           A new way to make the payments easy, reliable and secure.
         </p>
       </div>
-      <div className="flex01[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10">
+      <div className="flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10">
         {footerLinks.map((footerlink)=>(
-          <div key={footerlink.title} className={`flex flex-col ss:my-0 my-4 min-[150px]`}>
+          <div key={footerlink.title} className="flex flex-col ss:my-0 my-4 min-w-[150px]">
             <h4 className="font-poppins font-medium text-[18px] leading-[27px] text-white">
               {footerlink.title}
             </h4>
+            <ul className="list-none mt-4">
+              {footerlink.links.map((link,index)=>(
+                <li key={link.name} className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary cursor-pointer ${index !== footerlink.links.length - 1 ? "mb-4" : "mb-0"}`}>
+                  {link.name}
+                </li>
+              ))}
+            </ul>
           </div>
         ))}
       </div>
+    </div>
+    <div className="w-full justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t">
+
     </div>
   </section>
 );
