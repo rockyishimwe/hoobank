220cade73be5d9ec4c62ba2c0ebe3d9eeebc7a05
rocky ishimwe <rockyishimwe9@gmail.com>
2026-05-29 10:08:13 +0200

    changed some information in constant file

--- src/constants/index.js
+++ src/constants/index.js
@@ -56,7 +56,7 @@ export const feedback = [
     id: "feedback-2",
     content:
       "Money makes your life easier. If you're lucky to have it, you're lucky.",
-    name: "Steve Mark",
+    name: "Awk-3",
     title: "Founder & Leader",
     img: people02,
   },
@@ -159,16 +159,6 @@ export const socialMedia = [
     id: "social-media-1",
     icon: instagram,
     link: "https://www.instagram.com/",
-  },
-  {
-    id: "social-media-2",
-    icon: facebook,
-    link: "https://www.facebook.com/",
-  },
-  {
-    id: "social-media-3",
-    icon: twitter,
-    link: "https://www.twitter.com/",
   },
   {
     id: "social-media-4",
